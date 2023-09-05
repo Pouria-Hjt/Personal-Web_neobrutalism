844a033509888bcc10479e20010712c8d0e92d2a
pouria-hjt <poriw.hjt@gmail.com>
2023-09-06 01:44:10 +0330

    add some responsive to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,9 @@ import ResumeDataBox from "./ResumeDataBox/ResumeDataBox";
 const Header = () => {
   return (
     <header className="w-full h-[calc(100vh-96px)] bg-gradient-to-r bg-gradient__header flex justify-center items-center">
-      <div className="w-2/5 lg:w-4/5 h-4/5 bg-septenary rounded-3xl border-[3px] border-black shadow-secondary-shadow shadow-[#53AFB6] pt-8 relative">
+      <div className="w-2/5 lg:w-4/5 h-4/5 md:h-2/3 bg-septenary rounded-3xl border-[3px] border-black shadow-secondary-shadow md:shadow-primary-shadow md:shadow-[#53AFB6] shadow-[#53AFB6] pt-8 relative">
         <ResumeDataBox />
-        <div className="w-full h-14 gap-6 pl-8 flex justify-start flex-row-reverse items-center">
+        <div className="w-full h-14 gap-6 pl-8 md:pl-1 md:px-1 flex justify-start flex-row-reverse items-center">
           <MacCircleContainer />
           <WebUrl />
         </div>
--- src/components/Header/WebUrl/WebUrl.tsx
+++ src/components/Header/WebUrl/WebUrl.tsx
@@ -1,13 +1,34 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const WebUrl = () => {
   return (
-    <div className='flex flex-row-reverse gap-2 bg-[#F9F9F9] border-2 border-black px-4 py-3 rounded-xl'>
-        <Image src={'/close-square.svg'} alt='close' width={24} height={24}/>
-        <p>poriw.me - front-end developer</p>
+    <div className=" md:w-full flex flex-row-reverse gap-2 bg-[#F9F9F9] border-2 border-black px-4 py-3 rounded-xl">
+      <Image
+        src={"/close-square.svg"}
+        alt="close"
+        width={24}
+        height={24}
+        className="md:hidden"
+      />
+      <p className="text-sm">poriw.me - front-end developer</p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        className="md:inline-block hidden"
+        fill="none">
+        <path
+          d="M22 12C22 17.52 17.52 22 12 22C6.48 22 3.11 16.44 3.11 16.44M3.11 16.44H7.63M3.11 16.44V21.44M2 12C2 6.48 6.44 2 12 2C18.67 2 22 7.56 22 7.56M22 7.56V2.56M22 7.56H17.56"
+          stroke="#292D32"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
     </div>
-  )
-}
+  );
+};
 
-export default WebUrl
+export default WebUrl;
