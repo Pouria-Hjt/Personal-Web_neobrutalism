1b8e492e2590cf1f23dc27882b6181bfee4c6c05
pouria-hjt <poriw.hjt@gmail.com>
2023-09-06 02:46:25 +0330

    test tailwind render

--- src/components/Knowledge/Tech/Tech.tsx
+++ src/components/Knowledge/Tech/Tech.tsx
@@ -4,6 +4,17 @@ interface TechProps {
   percent: number;
 }
 const Tech = ({ name, percent }: TechProps) => {
+  const className =
+    "before:w-[10%]" ||
+    "before:w-[20%]" ||
+    "before:w-[30%]" ||
+    "before:w-[40%]" ||
+    "before:w-[50%]" ||
+    "before:w-[60%]" ||
+    "before:w-[70%]" ||
+    "before:w-[80%]" ||
+    "before:w-[90%]";
+
   return (
     <div className="w-64 md:w-11/12 h-fit flex flex-col justify-end items-end gap-2">
       <p>{name}</p>
