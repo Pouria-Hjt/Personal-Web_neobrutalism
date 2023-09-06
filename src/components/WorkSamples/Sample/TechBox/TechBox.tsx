095758e2619eb9a7e26029fbc017b3ed2b956c95
pouria-hjt <poriw.hjt@gmail.com>
2023-09-06 12:15:07 +0330

    change textbox bg color

--- src/components/WorkSamples/Sample/TechBox/TechBox.tsx
+++ src/components/WorkSamples/Sample/TechBox/TechBox.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TechBox = ({ name }: { name: string }) => {
   return (
-    <div className="w-20 h-7 rounded bg-tertiary flex justify-center items-center text-white text-sm">
+    <div className="w-20 h-7 rounded bg-quinary flex justify-center items-center text-white text-sm">
       {name}
     </div>
   );
